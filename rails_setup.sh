d0e0e65b289e1197ba580da9ca54af00471e231e
qdx <qdxzzz@gmail.com>
2013-11-20 23:21:52 -0800

    fixed rails_setup bug

--- rails_setup.sh
+++ rails_setup.sh
@@ -1,14 +1,14 @@
 # init rails dev environment with ruby 2.0.0 and rails 4.0.0
 cd ~/
 # install rvm
-\curl -L https://get.rvm.io | bash -s stable
-echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >>~/.bashrc
+curl -L https://get.rvm.io | bash -s stable
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
 source ~/.bashrc
 rvm -v
 
 # install ruby
 rvm install 2.0.0
-rvim use 2.0.0 --default
+rvm use 2.0.0 --default
 ruby -v
 
 # install rails
